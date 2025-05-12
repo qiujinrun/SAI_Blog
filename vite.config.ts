e2b67194cba0f5f52887ebb2b0d388cebfb6edbb
qiujinrun <13307743376@163.com>
2025-05-12 18:09:21 +0800

    修改网址

--- vite.config.ts
+++ vite.config.ts
@@ -1,24 +1,29 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import path from 'path'
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [vue()],
-  resolve:{
-    alias:{
-      "@":path.resolve(__dirname,'./src')
-    }
-  },
-  server: {
-    host: '0.0.0.0',
-    port: 8080,
-    open: true,
-    proxy: {
-      '/api': {
-        target: 'http://10.33.52.230:8081',
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, '')
-      }
-    }
+
+export default defineConfig(({ mode }) => {
+  // 加载 .env 文件中的变量
+  const env = loadEnv(mode, process.cwd())
+
+  return {
+    plugins: [vue()],
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, './src'),
+      },
+    },
+    server: {
+      host: '0.0.0.0',
+      port: 8080,
+      open: true,
+      proxy: {
+        '/api': {
+          target: env.VITE_API_URL, // 正确获取 VITE_API_URL
+          changeOrigin: true,
+          rewrite: (path) => path.replace(/^\/api/, ''),
+        },
+      },
+    },
   }
 })
